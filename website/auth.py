bb949088b15d415e5b1d0a468fd87bc17b0b4d91
ericareven <erica.reven@gmail.com>
2023-11-16 15:28:49 -0500

    inital route

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,3 @@
 from flask import Blueprint # this file is a blueprint of our app that has all of the routes for our website
 
-views = Blueprint('views', __name__)
-
-@views.route('/') # homepage url
-def home():
-    return '<h1>Test</>'
+auth = Blueprint('auth', __name__)
