b8bb2b97c129abe713fc618605023482cf20a34b
ericareven <erica.reven@gmail.com>
2023-11-17 15:17:15 -0500

    base.html nav

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,15 @@
 from flask import Blueprint # this file is a blueprint of our app that has all of the routes for our website
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/register')
+def register():
+    return "<p>Register</p>"
