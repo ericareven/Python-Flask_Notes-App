4186e34cb56cb300b5c1a3e6d63ef4ef1cb42465
ericareven <erica.reven@gmail.com>
2023-11-28 16:06:20 -0600

    debugged and auth routes

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint # this file is a blueprint of our app that has all of the routes for our website
+from flask import Blueprint, render_template # this file is a blueprint of our app that has all of the routes for our website
 
 views = Blueprint('views', __name__)
 
 @views.route('/') # homepage url
 def home():
-    return '<h1>Test</>'
+    return render_template("home.html")
+
