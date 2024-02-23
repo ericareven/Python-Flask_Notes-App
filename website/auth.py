e9d838e430ed9e77875e3d362d3121b58d801f1d
ericareven <erica.reven@gmail.com>
2024-02-23 10:37:41 -0600

    models

--- website/auth.py
+++ website/auth.py
@@ -23,9 +23,9 @@ def register():
         if len(email) < 4:
             flash('Email must be greater than 3 characters,', category='error')
         elif len(firstName) < 2:
-            flash('First name must be greater than 1 characters,', category='error')
+            flash('First name must be greater than 1 character,', category='error')
         elif len(password1) < 7:
-            flash('Password must be greater than 6 characters,', category='error')
+            flash('Password must be at least 7 characters,', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         else:
--- website/models.py
+++ website/models.py
@@ -1,1 +1,18 @@
+from . import db # from website
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now()) # func = get today's date
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # one(user) to many(notes) relationship: one object(parent) has many children
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True) # email max length (150 characters) and no two users can have thte same email
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
 
