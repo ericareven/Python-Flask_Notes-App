5d18e36ebc17196b7de0e8b92a11d5a2f7041c4d
ericareven <erica.reven@gmail.com>
2024-02-23 11:23:07 -0600

    database

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'secretkey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{path.join(path.dirname(path.abspath(__file__)), DB_NAME)}'
+    # CANNOT USE -> app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # bc When you specify the SQLALCHEMY_DATABASE_URI as sqlite:///database.db, Flask will create the database.db file in the instance folder if it doesn't exist
     db.init_app(app)
 
     from .views import views
@@ -16,4 +20,22 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/') # we do ont want any prefix, so not /auth
 
+    from .models import Note, User 
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database')
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash # this file is a blueprint of our app that has all of the routes for our website
+from flask import Blueprint, render_template, request, flash, redirect, url_for # this file is a blueprint of our app that has all of the routes for our website
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -16,19 +19,23 @@ def logout():
 def register():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('first_name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
         if len(email) < 4:
             flash('Email must be greater than 3 characters,', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 character,', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters,', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         else:
-             # add user to datatbase
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha384')) # sha256 is a hashing algorithm
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+
     return render_template("register.html")
